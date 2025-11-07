6ed96ceb858f1d8876fab9275b94ce7f0863d907
dbogdano <dbogdanoff@hotmail.com>
2025-11-06 16:09:47 -0800

    added classes back to tagfastq_mpi..py

--- src/blr/cli/tagfastq_mpi.py
+++ src/blr/cli/tagfastq_mpi.py
@@ -285,15 +285,322 @@ def setup_output(*args, **kwargs):
     """Wrapper around existing Output class initialization"""
     return Output(*args, **kwargs)
 
-# Include all existing classes (BarcodeReader, Output, ChunkHandler) and helper functions 
-# from the original tagfastq.py...
+
+class BarcodeReader:
+    def __init__(self, filename):
+        self._cache = {}
+        self._file = dnaio.open(filename, mode="r")
+        self.barcodes = iter(self._file)
+
+    def get_barcode(self, read_name, maxiter=128):
+        if read_name in self._cache:
+            return self._cache.pop(read_name)
+
+        for barcode in islice(self.barcodes, maxiter):
+            barcode_id = barcode.name.partition(" ")[0]
+            # If read_name in next pair then parser lines are synced --> drop cache.
+            if read_name == barcode_id:
+                self._cache.clear()
+                return barcode.sequence
+
+            self._cache[barcode_id] = barcode.sequence
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self._file.close()
+
+class Output:
+    """
+    Output handler for different output formats required by different mappers.
+    """
+    BIN_FASTQ_TEMPLATE = "ema-bin-*"  # Same name as in `ema preproc`.
+
+    def __init__(self, file1=None, file2=None, interleaved=False, file_nobc1=None, file_nobc2=None, mapper=None,
+                 bins_dir=None, nr_bins=None):
+        self._mapper = mapper
+
+        self._bin_nr = 0
+        self._reads_written = 0
+        self._bin_size = None
+        self._bins_dir = bins_dir
+        self._nr_bins = nr_bins
+        self._barcode_bin_map = {}
+        self._open_bins = None
+        self._prev_heap = None
+        self._bin_filled = True
+        self._pre_write = lambda *args: None
+        self._post_write = lambda *args: None
+
+        self._open_file = None
+        if file1 is not None:
+            self._open_file = self._setup_single_output(file1, file2, interleaved)
+        elif bins_dir is not None:
+            self._pre_write = self._open_new_bin_if_full
+            self._post_write = self._check_bin_full
+        else:
+            sys.exit("Either file1 or bins_dir need to be provided.")
+
+        self._open_file_nobc = None
+        if file_nobc1 is not None:
+            self._open_file_nobc = self._setup_single_output(file_nobc1, file_nobc2,
+                                                             interleaved=file_nobc2 is None)
+
+        # Setup writers based on mapper.
+        if self._mapper == "lariat":
+            self._write = self._write_lariat
+            self._write_nobc = self._write_nobc_lariat
+        else:
+            self._write = self._write_default
+            self._write_nobc = self._write_nobc_default
+
+    def set_bin_size(self, value):
+        self._bin_size = value
+
+    def _setup_single_output(self, file1, file2, interleaved):
+        if self._mapper == "lariat":
+            if file2 is not None:
+                Path(file2).touch()
+            return xopen(file1, mode='w')
+        else:
+            return dnaio.open(file1, file2=file2, interleaved=interleaved, mode="w", fileformat="fastq")
+
+    def _get_bin_name(self):
+        bin_nr_str = str(self._bin_nr).zfill(3)
+        file_name = self._bins_dir / Output.BIN_FASTQ_TEMPLATE.replace("*", bin_nr_str)
+        self._bin_nr += 1
+        return file_name
+
+    def _open_new_bin(self):
+        if self._open_file is not None:
+            self._open_file.close()
+
+        file_name = self._get_bin_name()
+        self._open_file = dnaio.open(file_name, interleaved=True, mode="w", fileformat="fastq")
+
+    def _open_new_bin_if_full(self, heap):
+        # Start a new bin if the current is full while not splitting heaps over separate bins
+        if self._bin_filled and heap != self._prev_heap:
+            self._bin_filled = False
+            self._open_new_bin()
+            logger.debug(f"Bin overflow = {self._reads_written} ")
+
+        self._prev_heap = heap
+
+    def _open_next_bin(self):
+        if self._open_bins is None:
+            # Open all bins
+            self._open_bins = []
+            for i in range(self._nr_bins):
+                file_name = self._get_bin_name()
+                self._open_bins.append(xopen(file_name, "w"))
+
+            self._open_bins = cycle(self._open_bins)
+
+        self._open_file = next(self._open_bins)
+
+    def _check_bin_full(self):
+        if self._reads_written > self._bin_size:
+            self._bin_filled = True
+            self._reads_written = 0
+
+    def _write_default(self, read1, read2):
+        self._open_file.write(read1, read2)
+
+    def _write_lariat(self, read1, _):
+        self._open_file.write(read1)
+
+    def _write_ema_special(self, read1, read2, barcode):
+        line = f"{barcode} @{read1.name} {read1.sequence} {read1.qualities} {read2.sequence} {read2.qualities}\n"
+        self._open_file.write(line)
+
+    def write(self, read1, read2=None, heap=None):
+        self._pre_write(heap)
+
+        # Write reads to output file
+        self._write(read1, read2)
+        self._reads_written += 1
+
+        self._post_write()
+
+    def write_ema_special(self, read1, read2, barcode):
+        assert self._nr_bins is not None, "Please set nr_bins to use ema special format"
+        self._open_file = self._barcode_bin_map.get(barcode)
+        if self._open_file is None:
+            self._open_next_bin()
+            self._barcode_bin_map[barcode] = self._open_file
+
+        self._write_ema_special(read1, read2, barcode)
+
+    def _write_nobc_default(self, read1, read2):
+        self._open_file_nobc.write(read1, read2)
+
+    def _write_nobc_lariat(self, read1, _):
+        self._open_file_nobc.write(read1)
+
+    def write_nobc(self, read1, read2=None):
+        if self._open_file_nobc is not None:
+            self._write_nobc(read1, read2)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._open_file is not None:
+            self._open_file.close()
+        if self._open_file_nobc is not None:
+            self._open_file_nobc.close()
+
+        if self._open_bins is not None:
+            for file in self._open_bins:
+                if file.closed:
+                    break
+                else:
+                    file.close()
+
+class ChunkHandler:
+    def __init__(self, tmpdir, chunk_size: int = 100_000):
+        # Required for ema sorted output
+        # Inspired by: https://stackoverflow.com/questions/56948292/python-sort-a-large-list-that-doesnt-fit-in-memory
+        self._output_chunk = []
+        self._chunk_size = chunk_size
+        self._chunk_id = 0
+        self._tmpdir = Path(tempfile.mkdtemp(prefix="tagfastq_sort", dir=tmpdir))
+        self._chunk_file_template = "chunk_*.tsv"
+        self._chunk_files = []
+        self._chunk_sep = "\t"
+        self._tmp_writer = self.create_writer()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._tmp_writer.close()
+
+    def create_writer(self):
+        if hasattr(self, "_tmp_writer"):
+            self._tmp_writer.close()
+        tmpfile = self._tmpdir / self._chunk_file_template.replace("*", str(self._chunk_id))
+        self._chunk_files.append(tmpfile)
+        self._chunk_id += 1
+        return open(tmpfile, "w")
+
+    def build_chunk(self, line: str):
+        """Add entry to write to temporary file, first argument should be the heap index"""
+        self._output_chunk.append(line)
+
+        if len(self._output_chunk) > self._chunk_size:
+            self.write_chunk()
+            self._tmp_writer = self.create_writer()
+
+    def write_chunk(self):
+        self._output_chunk.sort(key=self._get_heap)
+        self._tmp_writer.writelines(self._output_chunk)
+        self._output_chunk *= 0  # Clear list faster than list.clear(). See https://stackoverflow.com/a/44349418
+
+    def parse_chunks(self):
+        if not self._tmp_writer.closed:
+            self._tmp_writer.close()
+
+        with ExitStack() as chunkstack:
+            logger.info("Opening chunks for merge")
+            chunks = [chunkstack.enter_context(chunk.open(mode="r")) for chunk in self._chunk_files]
+            logger.info("Merging chunks")
+            for entry in merge(*chunks, key=self._get_heap):
+                yield entry.strip().split(self._chunk_sep)
+
+    def _get_heap(self, x):
+        return int(x.split(self._chunk_sep)[0])
+
 
 def add_arguments(parser):
-    # Add all existing arguments from original tagfastq.py
+    parser.add_argument(
+        "uncorrected_barcodes",
+        help="FASTQ/FASTA for uncorrected barcodes."
+    )
+    parser.add_argument(
+        "corrected_barcodes",
+        help="FASTQ/FASTA for error corrected barcodes. Currently accepts output from starcode "
+             "clustering with '--print-clusters' enabled."
+    )
+    parser.add_argument(
+        "input1",
+        help="Input FASTQ/FASTA file. Assumes to contain read1 if given with second input file. "
+             "If only input1 is given, input is assumed to be an interleaved. If reading from stdin"
+             "is requested use '-' as a placeholder."
+    )
+    parser.add_argument(
+        "input2", nargs='?',
+        help="Input  FASTQ/FASTA for read2 for paired-end read. Leave empty if using interleaved."
+    )
+    output = parser.add_mutually_exclusive_group(required=False)
+    output.add_argument(
+        "--output1", "--o1",
+        help="Output FASTQ/FASTA file name for read1. If not specified the result is written to "
+             "stdout as interleaved. If output1 given but not output2, output will be written as "
+             "interleaved to output1."
+    )
+    parser.add_argument(
+        "--output2", "--o2",
+        help="Output FASTQ/FASTA name for read2. If not specified but --o1/--output1 given the "
+             "result is written as interleaved."
+    )
+    parser.add_argument(
+        "--output-nobc1", "--n1",
+        help="Only for ema! Output FASTQ/FASTA file name to write non-barcoded read1 reads. "
+             "If output_nobc1 given but not output_nobc2, output will be written as interleaved to "
+             "output_nobc1."
+    )
+    parser.add_argument(
+        "--output-nobc2", "--n2",
+        help="Only for ema! Output FASTQ/FASTA file name to write non-barcoded read2 reads."
+    )
+    output.add_argument(
+        "--output-bins",
+        help=f"Output interleaved FASTQ split into bins named '{Output.BIN_FASTQ_TEMPLATE}' in the provided "
+             f"directory. Entries will be grouped based on barcode. Only used for ema mapping."
+    )
+    parser.add_argument(
+        "--nr-bins", type=int, default=100,
+        help="Number of bins to split reads into when using the '--output-bins' alternative. Default: %(default)s."
+    )
+    parser.add_argument(
+        "--tmpdir", type=str, default="./",
+        help="tmpdir"
+    )
+    parser.add_argument(
+        "-b", "--barcode-tag", default="BX",
+        help="SAM tag for storing the error corrected barcode. Default: %(default)s."
+    )
+    parser.add_argument(
+        "-s", "--sequence-tag", default="RX",
+        help="SAM tag for storing the uncorrected barcode sequence. Default: %(default)s."
+    )
+    parser.add_argument(
+        "-m", "--mapper", default="bowtie2", choices=ACCEPTED_READ_MAPPERS,
+        help="Specify read mapper for labeling reads with barcodes. Selecting 'ema' or 'lariat' produces output "
+             "required for these particular mappers. Default: %(default)s."
+    )
+    parser.add_argument(
+        "-c", "--min-count", default=0, type=int,
+        help="Minimum number of reads per barcode to tag read name. Default: %(default)s."
+    )
+    parser.add_argument(
+        "-p", "--pattern-match",
+        help="IUPAC barcode string to match against corrected barcodes e.g. for DBS it is usualy "
+             "BDHVBDHVBDHVBDHVBDHV. Non-matched barcodes will be removed."
+    )
+    parser.add_argument(
+        "--sample-nr", type=int, default=1,
+        help="Sample number to append to barcode string. Default: %(default)s."
+    )
     parser.add_argument(
         "--mpi-chunk-size",
         type=int,
         default=1000000,
         help="Number of reads to process in each MPI chunk. Default: %(default)s"
     )
-    # Include all other existing arguments...
